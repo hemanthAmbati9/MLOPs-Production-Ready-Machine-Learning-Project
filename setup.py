d55580144b6a64e7dc687eda861e99267d858a68
hemanthAmbati9 <hemanthambati9@gmail.com>
2025-10-26 15:25:09 +0000

    requirments added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="hemanthAmbati",
+    author_email="hemanthambati9@gmail.com",
+    packages=find_packages()
+)
